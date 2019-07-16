34c7389003b3e49c5d3909f0402950ccb9e00d29
Diana <dianadiasds@gmail.com>
2019-07-16 15:01:17 +0200

    Scoreboard incrementScore and style

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
-import Player from './Player'
+import React, {Component} from "react";
+import Player from "./Player";
+import "./Scoreboard.css";
 
 export default class Scoreboard extends Component {
   state = {
@@ -9,16 +10,42 @@ export default class Scoreboard extends Component {
       { id: 3, name: "Mimi",  score: 4 },
     ]
   };
-  render(){
+
+  incrementScoreOfPlayer = id => {
+
+    const updatedPlayers = this.state.players.map(player => {
+      if (player.id === id) {
+        return { ...player, score: player.score + 1 };
+      } else {
+        return player;
+      }
+    });
+    this.setState({ players: updatedPlayers });
+  }
+
+  render () {
+    const players_copy = [ ...this.state.players ];
+    players_copy.sort((a, b) => b.score - a.score);
+
     return (
       <div className="scoreboard">
         <h1>Scoreboard</h1>
         <ul>
-            {this.state.players.map((player) => {
-              return <Player name={player.name} key={player.id} score={player.score}/>
-            })}
+          {players_copy.map(this.renderPlayer)}
         </ul>
       </div>
-    )
+    );
+  }
+
+  renderPlayer = player => {
+    return (
+      <Player
+        id={player.id}
+        name={player.name}
+        score={player.score}
+        key={player.id}
+        incrementScore={this.incrementScoreOfPlayer}
+      />
+    );
   }
 }
